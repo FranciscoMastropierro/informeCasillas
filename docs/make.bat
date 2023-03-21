a35a790ce23203abe931217c780da21180e3b8bd
Francisco Mastropierro <mastropierro.francisco@gmail.com>
2023-03-21 15:22:03 -0300

    Fix make.bat set BUILDDIR=_build

--- docs/make.bat
+++ docs/make.bat
@@ -8,7 +8,7 @@ if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD="C:\Users\Usuario\AppData\Local\Programs\Python\Python311\Scriptssphinx-build.exe"
 )
 set SOURCEDIR=.
-set BUILDDIR=_build
+set BUILDDIR=build
 
 %SPHINXBUILD% >NUL 2>NUL
 if errorlevel 9009 (
