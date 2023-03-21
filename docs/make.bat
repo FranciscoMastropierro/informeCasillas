1dff238e9b70e5d616ebdd1b8b456127eb40e2ce
Francisco Mastropierro <mastropierro.francisco@gmail.com>
2023-03-21 14:57:20 -0300

    Fix make.bat

--- docs/make.bat
+++ docs/make.bat
@@ -5,7 +5,7 @@ pushd %~dp0
 REM Command file for Sphinx documentation
 
 if "%SPHINXBUILD%" == "" (
-	set SPHINXBUILD=sphinx-build
+	set SPHINXBUILD="C:\Users\Usuario\AppData\Local\Programs\Python\Python311\Scriptssphinx-build.exe"
 )
 set SOURCEDIR=.
 set BUILDDIR=_build
